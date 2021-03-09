860eaf9fd8dfde930656e21fb0fe0f35743ed83c
iuriasydes <iuriasydes@gmail.com>
2021-03-09 14:56:43 -0800

    Corrigiendo rutas las ruta de los archivos JS2

--- restaurant/js/modal.js
+++ restaurant/js/modal.js
@@ -1,6 +1,18 @@
-let btnMenu = document.getElementById('btnmenu');
-let menu = document.getElementById('menu');
-btnMenu.addEventListener('click',function(){
-    'use strict';
-    menu.classList.toggle('mostrar');
+let imagenes = document.querySelectorAll('.gallery__img');
+let modal = document.querySelector('#modal');
+let img = document.querySelector('#modal__img');
+let boton = document.querySelector('#modal__boton');
+
+//para agregar la url de la imagen y mostrar la imagen
+for(let i=0; i<imagenes.length; i++){
+  imagenes[i].addEventListener('click',function(e){
+    modal.classList.toggle("modal--open");//agrega la clase a modal
+    let src = e.target.src;
+    img.setAttribute("src",src);
+  });
+}
+
+//para cerrar el modal con la X
+boton.addEventListener('click',function(){
+  modal.classList.toggle("modal--open");
 });
